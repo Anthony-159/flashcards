3a0c3df14b6aaeef52fd6ba048078e0836f1a355
Anthony-159 <169106304+Anthony-159@users.noreply.github.com>
2024-11-08 21:06:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Quais as quatro operações básicas da matemática?',
+    'Adição, Subtração, Multiplicação e Divisão'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'História',
+    'Qual o periodo da Primeira Guerra Mundial?',
+    'Ela durou de 28 de julho de 1914 até 11 de novembro de 1918'
+)
+
+criaCartao(
+    'Biologia',
+    'Existe algum mamifero ovíparo?',
+    'Sim, as equidnas são um exemplo'
+)
